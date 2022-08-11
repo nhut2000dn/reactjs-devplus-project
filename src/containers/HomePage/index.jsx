c983ee7b3fa4d688c7358b14a3ac9c1a863ebc47
nhutnguyen2000 <nhut.nguyen@devplus.edu.vn>
2022-08-11 14:52:04 +0700

    new feature about and responsive

--- src/containers/HomePage/index.jsx
+++ src/containers/HomePage/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../../components/Header'
 import Banner from '../../components/Banner'
+import About from '../../components/About'
 
 const HomePage = () => {
   return (
     <div>
       <Header/>
       <Banner/>
+      <About/>
     </div>
   )
 }
